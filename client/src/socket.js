24fb165864fc22edfbf0149a6aca77f71b9c9d47
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-05-05 19:41:37 +0200

    socketJS updated changed from custom to document.location.orign

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,3 @@
 import { io } from "socket.io-client";
 
-const URL =
-  process.env.NODE_ENV === "production"
-    ? "https://c46-group-a-f3ebdee28d59.herokuapp.com/"
-    : "http://localhost:3000";
-
-export const socket = io(URL);
+export const socket = io(document.location.origin);
