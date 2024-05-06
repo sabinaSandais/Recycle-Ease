ecd9603d9e60a9f6a8e3fbccbd4adbdb9ee441b0
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-05-06 21:12:52 +0200

    added navbar and login logout navbar: added top with background login & signUp is button and form is added

--- client/src/components/navbar/Nav.jsx
+++ client/src/components/navbar/Nav.jsx
@@ -1,29 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
+
+//components
+import UserFavicon from "../users/userFavicon/UserFavicon";
+import Login from "../users/login/login";
+import SignUp from "../users/signup/SignUp";
 
 import TEST_ID from "../Nav.testid";
 
 const Nav = () => {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showSignUpForm, setShowSignUpForm] = useState(false);
+
   return (
-    <div className="navbar">
-      <ul className="navbar-items">
-        <li className="navbar-item">
-          <Link to="/" data-testid={TEST_ID.linkToHome} className="navbar-link">
-            Home
-          </Link>
-        </li>
-        <li className="navbar-item">
-          <Link
-            to="/user"
-            data-testid={TEST_ID.linkToUsers}
-            className="navbar-link"
+    <>
+      <div className="navbar">
+        <ul className="navbar-items">
+          <li className="navbar-item">
+            <Link
+              to="/"
+              data-testid={TEST_ID.linkToHome}
+              className="navbar-link"
+            >
+              Home
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link
+              to="/user"
+              data-testid={TEST_ID.linkToUsers}
+              className="navbar-link"
+            >
+              Users
+            </Link>
+          </li>
+          <li
+            className="navbar-item login-btn"
+            onClick={() => {
+              if (showSignUpForm) {
+                setShowSignUpForm(!showSignUpForm);
+              }
+              setShowLoginForm(!showLoginForm);
+            }}
           >
-            Users
-          </Link>
-        </li>
-      </ul>
-    </div>
+            <a href="#" className="navbar-link">
+              Log In
+            </a>
+          </li>
+          <li
+            className="navbar-item signUp-btn"
+            onClick={() => {
+              if (showLoginForm) {
+                setShowLoginForm(!showLoginForm);
+              }
+              setShowSignUpForm(!showSignUpForm);
+            }}
+          >
+            <a href="#" className="navbar-link">
+              Sign Up
+            </a>
+          </li>
+          <UserFavicon />
+        </ul>
+      </div>
+      <Login showLoginForm={showLoginForm} />
+      <SignUp showSignUpForm={showSignUpForm} />
+    </>
   );
 };
 
