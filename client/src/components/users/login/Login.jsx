04835d604b457f29b0776e28b51d0e9ad219d719
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-05-06 21:12:52 +0200

    added navbar and login logout navbar: added top with background login & signUp is button and form is added

---
+++ client/src/components/users/login/Login.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./login.css";
+
+function Login({ showLoginForm }) {
+  return showLoginForm && showLoginForm === true ? (
+    <div className="login-container">
+      <form className="login-form">
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  ) : null;
+}
+
+Login.propTypes = {
+  showLoginForm: PropTypes.bool,
+};
+
+export default Login;
