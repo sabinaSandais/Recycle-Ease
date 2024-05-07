6aa03c7ac7a1d2f7ed56b63440108c2aa21a9f6f
erhan <erhan.16k@gmail.com>
2024-05-07 12:02:31 +0200

    socket definition changed to localhost:3000

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,4 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+export const socket = io(document.location.origin);
+//export const socket = io("http://localhost:3000");
