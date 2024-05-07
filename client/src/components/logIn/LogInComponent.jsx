ca85cbddd4bf0a112fcf877f80d4d3208234b0a1
Tenzin Kunchok <145247192+Tenzijn@users.noreply.github.com>
2024-05-08 01:19:30 +0200

    userAPI connected to frontend

--- client/src/components/logIn/LogInComponent.jsx
+++ client/src/components/logIn/LogInComponent.jsx
@@ -1,17 +1,69 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./login.css";
+import React, { useEffect, useState } from "react";
 
+import useFetch from "../../hooks/useFetch";
+import PropTypes from "prop-types";
+
+import "./login.css";
+import { logInfo } from "../../../../server/src/util/logging";
 function LogInComponent({ showLoginForm }) {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { isLoading, error, performFetch, cancelFetch } =
+    useFetch("/user/login");
+
+  useEffect(() => {
+    return cancelFetch;
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    performFetch({
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ name, password }),
+    });
+  };
+
+  let statusComponent = null;
+  if (error != null) {
+    if (error.error) {
+      statusComponent = <div>{error.error}</div>;
+    }
+    if (error.token && error.token !== undefined) {
+      logInfo("Token received: ", error.token);
+      localStorage.setItem("UserToken", error.token);
+      statusComponent = <div>Logged in successfully</div>;
+    }
+  } else if (isLoading) {
+    statusComponent = <div>Logging in....</div>;
+  }
+
   return showLoginForm && showLoginForm === true ? (
     <div className="login-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" />
+        <input
+          type="text"
+          id="username"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Login</button>
       </form>
+      {statusComponent}
     </div>
   ) : null;
 }
--- client/src/components/signUp/SignUpComponent.jsx
+++ client/src/components/signUp/SignUpComponent.jsx
@@ -1,19 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import useFetch from "../../hooks/useFetch";
 import PropTypes from "prop-types";
+import { logInfo } from "../../../../server/src/util/logging";
 import "./signUp.css";
 
 function SignUpComponent({ showSignUpForm }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSuccess = () => {
+    setName("");
+    setEmail("");
+    setPassword("");
+    logInfo("User created successfully");
+  };
+
+  const { isLoading, error, performFetch, cancelFetch } = useFetch(
+    "/user/",
+    onSuccess,
+  );
+
+  useEffect(() => {
+    return cancelFetch;
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    performFetch({
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ user: { name, email, password } }),
+    });
+  };
+
+  let statusComponent = null;
+  if (error != null) {
+    statusComponent = (
+      <div>Error while trying to create user: {error.toString()}</div>
+    );
+  } else if (isLoading) {
+    statusComponent = <div>Creating user....</div>;
+  }
+
   return showSignUpForm && showSignUpForm === true ? (
     <div className="signUp-container">
-      <form className="signUp-form">
+      <form className="signUp-form" onSubmit={handleSubmit}>
         <label htmlFor="username">User Name:</label>
-        <input type="text" id="username" name="username" />
+        <input
+          type="text"
+          id="username"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label htmlFor="email">E-mail:</label>
-        <input type="email" id="email" name="email" />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Sign Up</button>
       </form>
+      {statusComponent}
     </div>
   ) : null;
 }
