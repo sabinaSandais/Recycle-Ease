2887f5aab529a9d96d2a94dd79bcea30658d5319
Arkaitz Garro <arkaitz.garro@gmail.com>
2024-05-08 21:42:01 +0200

    chore: use window location for production

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,14 +1,8 @@
 import { io } from "socket.io-client";
 
-let URL = null;
-if (process.env.NODE_ENV === "development") {
-  URL = "http://localhost:3000";
-}
-
-if (process.env.NODE_ENV === "production") {
-  URL =
-    process.env.HEROKU_APP_DEFAULT_DOMAIN_NAME ||
-    "https://hyf-c46-grou-integrate--63liqp.herokuapp.com";
-}
+const URL =
+  process.env.NODE_ENV === "production"
+    ? window.location.origin
+    : "http://localhost:3000";
 
 export const socket = io(URL);
