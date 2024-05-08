c8fd613dcb2a6f6c8a2abf4d5830dc3784d0bcad
erhan <erhan.16k@gmail.com>
2024-05-08 13:43:17 +0200

    socket.js updated

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,7 @@
 import { io } from "socket.io-client";
-
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://c46groupa.hackyourfuture.tech"
+    : "http://localhost:3000";
 //export const socket = io(document.location.origin);
-export const socket = io("http://localhost:3000");
+export const socket = io(URL);
