9456a0ddb1f079492f86d6b3f5eb299ed962b11f
erhan <erhan.16k@gmail.com>
2024-05-08 15:08:47 +0200

    update socket.js to branch deployment addres

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://c46groupa.hackyourfuture.tech"
+    ? "https://hyf-c46-grou-integrate--63liqp.herokuapp.com"
     : "http://localhost:3000";
 //export const socket = io(document.location.origin);
 export const socket = io(URL);
