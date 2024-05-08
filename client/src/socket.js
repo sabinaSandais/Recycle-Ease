f0652ae6884dcdae6fea6444c8ed43dda5356e85
Arkaitz Garro <arkaitz.garro@gmail.com>
2024-05-08 21:39:53 +0200

    chore: dynamic domain hosts for Socket.io

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,14 @@
 import { io } from "socket.io-client";
 
-export const socket = io(document.location.origin);
+let URL = null;
+if (process.env.NODE_ENV === "development") {
+  URL = "http://localhost:3000";
+}
+
+if (process.env.NODE_ENV === "production") {
+  URL =
+    process.env.HEROKU_APP_DEFAULT_DOMAIN_NAME ||
+    "https://hyf-c46-grou-integrate--63liqp.herokuapp.com";
+}
+
+export const socket = io(URL);
