40af9ed2f23e3f1631a1a49744c476597661edfe
tibetdev <tibetdev@gmail.com>
2024-05-13 16:00:39 +0200

    added more demo automation close all machines open all machines toggle 50% of machines

--- server/src/routes/machine.js
+++ server/src/routes/machine.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { getMachines, updateMachineStatus } from "../controllers/machine.js";
+import {
+  getMachines,
+  updateMachineStatus,
+  turnOffAllMachines,
+  turnOnAllMachines,
+  toggleMachineStatus,
+} from "../controllers/machine.js";
 
 const machineRouter = express.Router();
 
 machineRouter.get("/", getMachines);
 machineRouter.post("/:id", updateMachineStatus);
+machineRouter.get("/turnOffAll", turnOffAllMachines);
+machineRouter.get("/turnOnAll", turnOnAllMachines);
+machineRouter.get("/toggleHalf", toggleMachineStatus);
 export default machineRouter;
