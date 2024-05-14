cc40c8e4d7d8db1aa00c4d460633191cdabca231
AJasem <ajassem.ahmad@gmail.com>
2024-05-14 19:49:54 +0200

    updated getreviews function to send the average score to the machines schema so we can display it in the favorite

--- server/src/controllers/reviews.js
+++ server/src/controllers/reviews.js
@@ -1,6 +1,6 @@
 import Review from "../models/reviews.js";
 import { logError } from "../util/logging.js";
-
+import Machine from "../models/Machine.js";
 export const createReview = async (req, res) => {
   try {
     const { comment, stars, machineId, userId } = req.body;
@@ -24,6 +24,9 @@ export const getReviews = async (req, res) => {
   try {
     const machineId = req.params.machineId;
     const reviews = await Review.find({ machine: machineId });
+    const machine = await Machine.findById(machineId);
+    machine.score = averageScore(reviews);
+    await machine.save();
     res.status(200).json({ success: true, result: reviews });
   } catch (error) {
     logError(error);
@@ -31,4 +34,9 @@ export const getReviews = async (req, res) => {
       .status(500)
       .json({ success: false, msg: "Unable to get reviews, try again later" });
   }
+};
+
+const averageScore = (reviews) => {
+  const total = reviews.reduce((acc, review) => acc + review.stars, 0);
+  return (total / reviews.length).toFixed(1);
 };
